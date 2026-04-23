f4258b8ee3e1e2fb366c1df0a89ad70279092a9e
unknown <ketlyvieirabarros@gmail.com>
2026-04-23 15:21:12 -0300

    app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,151 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+export default function App() {
+  const [peso, setPeso] = useState<string>("");
+  const [altura, setAltura] = useState<string>("");
+  const [idade, setIdade] = useState<string>("");
+  const [sexo, setSexo] = useState<"M" | "F">("M");
+  const [resultado, setResultado] = useState<string>("");
 
-export default function HomeScreen() {
+  const parseNumero = (valor: string): number =>{
+    return parseFloat(valor.replace(",","."));
+  };
+
+  const calcular = (): void =>{
+    const p = parseNumero(peso);
+    const a = parseNumero(altura);
+    const i = parseNumero(idade);
+
+    if (isNaN(p) || isNaN(a) || isNaN(i) || a <= 0){
+      setResultado("Preencha os dados corretamente");
+      return;
+    }
+
+    const imc = p / (a * a);
+
+    const gordura = 
+     sexo === "M"
+      ? (1.2 * imc) + (0.23 * i) - 16.2
+      : (1.2 * imc) + (0.23 * i) - 5.4;
+
+    setResultado(`${gordura.toFixed(2)}%`);
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    <View style={styles.container}>
+      <Text style={styles.title}>Calculadora de Gordura</Text>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <TextInput
+        placeholder="Peso (kg) Ex: 80"
+        keyboardType="decimal-pad"
+        value={peso}
+        onChangeText={setPeso}
+        style={styles.input}
+      />
+
+      <TextInput
+       placeholder="Altura (m) Ex: 1,75"
+       keyboardType="decimal-pad"
+       value={altura}
+       onChangeText={setAltura}
+       style={styles.input}
+      />
+
+      <TextInput
+       placeholder="Idade"
+       keyboardType="numeric"
+       value={idade}
+       onChangeText={setIdade}
+       style={styles.input}
+      />
+
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={[
+            styles.buttonSexo,
+            sexo === "M" && styles.buttonAtivo,
+          ]}
+          onPress={() => setSexo("M")}
+        >
+          <Text>Masculino</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            styles.buttonSexo,
+            sexo === "F" && styles.buttonAtivo,
+          ]}
+          onPress={() => setSexo ("F")}
+        >
+          <Text>Feminino</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.calcularBtn} onPress={calcular}>
+        <Text style={styles.calcularText}>Calcular</Text>
+      </TouchableOpacity>
+
+      {resultado !== "" && (
+        <Text style={styles.resultado}>{resultado}</Text>
+      )}
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      padding: 20,
+    },
+    title: {
+      fontSize: 22,
+      marginBottom: 20,
+      textAlign: "center",
+      fontWeight:"bold",
+    },
+    input: {
+      borderWidth: 1,
+      padding: 10,
+      marginBottom: 10,
+      borderRadius: 8,
+    },
+    row: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginBottom: 15,
+    },
+    buttonSexo: {
+      flex: 1,
+      padding: 10,
+      marginHorizontal: 5,
+      borderWidth: 1,
+      borderRadius: 8,
+      alignItems: "center",
+    },
+    buttonAtivo: {
+      backgroundColor: "#ddd",
+    },
+    calcularBtn: {
+      backgroundColor: "#333",
+      padding: 12,
+      borderRadius: 10,
+      alignItems:"center",
+    },
+    calcularText: {
+      color: "#fff",
+      fontWeight:"bold",
+    },
+    resultado: {
+      marginTop: 20,
+      textAlign: "center",
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+  });
